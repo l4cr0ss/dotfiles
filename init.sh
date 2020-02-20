bd2f7d51e4da209ddb35ddf6c998724a9829d2eb
Jefferson Hudson <jefferson.hudson@gmail.com>
2020-02-20 16:56:11 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+
+function status {
+    echo "[*]" $@
+}
+
+# Setup symlinks
+for file in $DIR/*; do
+    filename=$(basename $file)
+    if [[ "$filename" != "$(basename $0)" ]]; then
+        if [ -e $HOME/.$filename ]; then
+            if ! [ -h $HOME/.$filename ]; then
+                # remove existing dotfile
+		status "deleted existing file $HOME/.$filename"
+                rm $HOME/.$filename
+            fi
+        fi
+        status "installing $filename --> .$filename"
+        ln -s $DIR/$filename $HOME/.$filename
+    fi
+done
+
